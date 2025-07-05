4432afade2c238222dd5bc50ee69375f6f656c1e
marcelarie <googlillo@gmail.com>
2025-07-05 22:01:01 +0200

    add lua test flake

---
+++ lua/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Lua dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # pick exactly one interpreter; comment the other
+          (lua5_4.withPackages (ps: with ps; [luafilesystem luaposix]))
+          # luajit
+          lua-language-server # LSP for editors
+          luacheck # static analysis
+          stylua # formatter
+          luarocks # rock management
+          readline # nicer REPL history
+        ];
+
+        shellHook = ''
+          echo "Lua shell on ${pkgs.lua5_4.version} – happy hacking!"
+        '';
+      };
+    });
+}
